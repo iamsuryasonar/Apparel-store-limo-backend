1cac8180decc46ecb69f7200129d49a7f6a315e2
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-13 21:26:01 +0530

    fix: bug in card fixed

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose')
 const { success, error, validation } = require('../common/responseAPI')
 const Item = require('../models/Item')
 const Product = require('../models/Product')
+const async = require('async');
+const QUANTITY_LIMIT = 5;
 
-// @desc   Add to cart
-// @route   POST /api/v1/cart/
-// @access  Private/Customer
+// Create a queue with concurrency 1 (only one task at a time)
+const queue = async.queue(async (task, callback) => {
+    await task.funct(task.req, task.res);
+    // Callback to signal completion (worker is free to execute next task)
+    callback();
+}, 1);
 
-exports.addToCart = async (req, res) => {
-
+const addToCartQueue = async (req, res) => {
+    console.log('here')
+    let session;
     try {
-        let session = await mongoose.startSession();
+        session = await mongoose.startSession();
         session.startTransaction();
 
         let { quantity, productId, colorVariantId, sizeVariantId } = req.body;
@@ -19,22 +25,27 @@ exports.addToCart = async (req, res) => {
         const product = await Product.findById(productId);
         if (!product) return res.status(404).json(error("Product not found", res.statusCode));
 
-        let item = await Item.find({ sizevariant: sizeVariantId });
+        let item = await Item.findOne({ sizevariant: sizeVariantId, customer: customerId }).session(session);;
         let cart = [];
 
-        // max quantity limit reached
-        if (item && item?.length === 1 && item[0]?.quantity >= 5) return res.status(201).json(success("Can't purchase more than 5 items!", {
+        // quantity can't exceed quantity limit 
+        if (quantity > QUANTITY_LIMIT) return res.status(201).json(success("Can't purchase more than" + QUANTITY_LIMIT + "items!", {
             item
         },
             res.statusCode),
         );
 
-        // item already exist in cart
-        if (item.length === 1 && item[0]?.quantity < 5) {
-            item[0].quantity = item[0].quantity + 1;
-            cart = await item[0].save({ session });
-        }
-        if (item?.length === 0) {// if limit not reached and item don't already exists in the cart then, go ahead and create new Item
+        // if item already exists in cart and quantity is provided
+        if (item && quantity) {
+            console.log('item already exists', item.quantity, 'qty to add', quantity, 'Could add', QUANTITY_LIMIT - item.quantity)
+
+            if (quantity > QUANTITY_LIMIT - item.quantity) {//if quantity to add is greater than quantity could be added, replace quantity with quantity that could be added
+                quantity = QUANTITY_LIMIT - item.quantity;
+            }
+
+            item.quantity = item.quantity + Number(quantity);
+            cart = await item.save({ session });
+        } else { // if limit not reached and item doesn't already exist in the cart then, go ahead and create new Item
             cart = await Item.create(
                 [{
                     quantity,
@@ -48,10 +59,9 @@ exports.addToCart = async (req, res) => {
         }
 
         await session.commitTransaction();
-        session.endSession();
 
         res.status(201).json(success("OK", {
-            cart
+            // cart
         },
             res.statusCode),
         );
@@ -60,7 +70,78 @@ exports.addToCart = async (req, res) => {
         await session.abortTransaction();
         session.endSession();
         return res.status(500).json(error("Something went wrong", res.statusCode));
+    } finally {
+        session.endSession();
     }
+};
+
+
+const updateProductQuantityQueue = async (req, res) => {
+    let session;
+    try {
+        session = await mongoose.startSession();
+        session.startTransaction();
+
+        const { id } = req.params;
+        let { quantity } = req.body;
+        const customerId = req.user._id;
+
+        if (!quantity) return res.status(400).json(error("Quantity required!", res.statusCode));
+        if (quantity > 5) return res.status(400).json(error("Quantity should be less than 6", res.statusCode));
+
+        let cartItem = await Item.findOne({ _id: id, isOrdered: false, customer: customerId });
+        if (!cartItem) return res.status(404).json(error("Cart item not found", res.statusCode));
+
+
+        if (quantity === 0) {
+            let deletedItem = await Item.deleteOne({ _id: req.params.id, isOrdered: false }, { session });
+
+            await session.commitTransaction();
+
+            return res.status(201).json(success("OK", {
+                deletedItem
+            },
+                res.statusCode),
+            );
+        }
+
+        cartItem.quantity = quantity;
+        await cartItem?.save({ session });
+
+        await session.commitTransaction();
+
+        res.status(201).json(success("OK", {
+            cartItem
+        },
+            res.statusCode),
+        );
+    } catch (err) {
+        console.log(err)
+        await session.abortTransaction();
+        return res.status(500).json(error("Something went wrong", res.statusCode));
+    } finally {
+        session.endSession();
+    }
+};
+
+// @desc   Add to cart
+// @route   POST /api/v1/cart/
+// @access  Private/Customer
+
+// Function to handle addToCart requests one at a time to achieve ACID support in concurrent request
+exports.addToCart = async (req, res) => {
+    // Add the request and response objects to the queue
+    queue.push({ funct: addToCartQueue, req, res });
+};
+
+// @desc   Update quatify of item in cart
+// @route   PUT /api/v1/cart/:id
+// @access  Private/Customer
+
+// Function to handle addToCart requests one at a time to achieve ACID support in concurrent request
+exports.updateProductQuantity = async (req, res) => {
+    // Add the request and response objects to the queue
+    queue.push({ funct: updateProductQuantityQueue, req, res });
 };
 
 // @desc   Get all items in cart
@@ -89,62 +170,7 @@ exports.getAllItemsInCart = async (req, res) => {
     }
 };
 
-// @desc   Update quatify of item in cart
-// @route   PUT /api/v1/cart/:id
-// @access  Private/Customer
 
-exports.updateProductQuantity = async (req, res) => {
-    try {
-        let session = await mongoose.startSession();
-        session.startTransaction();
-
-        const { id } = req.params;
-        let { type, productId } = req.body;
-        const customerId = req.user._id;
-
-        const product = await Product.findById(productId);
-        if (!product) return res.status(404).json(error("Product not found", res.statusCode));
-
-        const item = await Item.findById(id);
-        if (!item) return res.status(404).json(error("Cart item not found", res.statusCode));
-
-        const cartItem = await Item.find({ _id: id, isOrdered: false, customer: customerId });
-        if (!cartItem) return res.status(404).json(error("Cart item not found", res.statusCode));
-
-        if (type === 'DECREMENT' && cartItem[0].quantity === 1) {
-            let deletedItem = await Item.deleteOne({ _id: req.params.id, isOrdered: false }, { session });
-
-            await session.commitTransaction();
-            session.endSession();
-
-            return res.status(201).json(success("OK", {
-                deletedItem
-            },
-                res.statusCode),
-            );
-        }
-
-        if (type === 'INCREMENT' && cartItem[0].quantity < 5) cartItem[0].quantity = cartItem[0].quantity + 1;
-
-        if (type === 'DECREMENT' && cartItem[0].quantity > 0) cartItem[0].quantity = cartItem[0].quantity - 1;
-
-        await cartItem[0].save({ session });
-
-        await session.commitTransaction();
-        session.endSession();
-
-        res.status(201).json(success("OK", {
-            cartItem
-        },
-            res.statusCode),
-        );
-    } catch (err) {
-        console.log(err)
-        await session.abortTransaction();
-        session.endSession();
-        return res.status(500).json(error("Something went wrong", res.statusCode));
-    }
-};
 
 // @desc    Remove item from cart
 // @route   PUT /api/v1/cart/:id
