a50a799befad6eed3d9327c371241460f25b1d4d
Surya Sonar <iamsuryasonar@gmail.com>
2024-02-14 03:29:33 +0530

    feat: get all published products api added

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -87,27 +87,124 @@ exports.getProductById = async (req, res) => {
 exports.getAllPublishedProducts = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 12;
     const skip = (page - 1) * limit;
+    console.log(page, limit, skip)
 
-    const products = await Product.find({ isPublished: true })
-      .populate({
-        path: 'colorvariants',
-        populate: ['images', 'sizevariants']
-      })
-      .skip(skip)
-      .limit(limit)
-      .exec();
+    const { from, to, sort_type } = req.query;
+    const products = await Product.aggregate([
+      {
+        $match: {
+          isPublished: true,// Match all published documents 
+        }
+      },
+      {
+        $lookup: { // Perform a lookup to fetch category details
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "category"
+        }
+      },
+      {// Unwind the category array to obtain individual category details
+        $unwind: "$category"
+      },
+      {// Lookup to fetch color variants related to the product
+        $lookup: {
+          from: "colorvariants",
+          localField: "_id",
+          foreignField: "product",
+          as: "colorVariants"
+        }
+      },
+      {// Unwind the colorVariants array
+        $unwind: "$colorVariants"
+      },
+      {// Lookup to fetch size variants based on color variants
+        $lookup: {
+          from: "sizevariants",
+          localField: "colorVariants._id",
+          foreignField: "colorVariant",
+          as: "sizeVariants"
+        }
+      },
+      {// Unwind the sizeVariants array
+        $unwind: "$sizeVariants"
+      },
+      {// Filter documents based on selling price within the specified range
+        $match: {
+          'sizeVariants.selling_price': {
+            $gt: Number(from),
+            $lt: Number(to)
+          }
+        }
+      },
+      {// Lookup to fetch images related to color variants
+        $lookup: {
+          from: "images",
+          localField: "colorVariants._id",
+          foreignField: "colorVariant",
+          as: "images"
+        }
+      },
+      {
+        $addFields: {
+          imagesCount: { $size: "$images" }, // Get the size of the images array and gets product information and assigns to keys
+          _id: "$_id",
+          name: "$name",
+          description: "$description",
+          tag: "$tag",
+          keyword: "$keyword",
+        }
+      },
+      {
+        $addFields: {
+          randomIndex: { $floor: { $multiply: ["$imagesCount", { $rand: {} }] } } // Generate a random index
+        }
+      },
+      {
+        $addFields: {
+          randomizedImages: { $arrayElemAt: ["$images", "$randomIndex"] } // Get the image at the random index
+        }
+      },
+      {
+        $project: {
+          category: 1,
+          colorVariants: 1,
+          sizeVariants: 1,
+          image: "$randomizedImages", // Assign the randomized image to the image
+          _id: 1,
+          name: 1,
+          description: 1,
+          tag: 1,
+          keyword: 1,
+        }
+      },
+      {
+        $facet: {// Use $facet to perform multiple aggregations within a single stage
+          count: [// Count the total number of matched documents
+            { $group: { _id: null, count: { $sum: 1 } } },
+          ],
+          // Retrieve matched results based on selling price range
+          matchedResults: [// Filter documents based on selling price range
+            { $match: { 'sizeVariants.selling_price': { $gt: Number(from), $lt: Number(to) } } },
+            { $sample: { size: 99999 } },// Randomly select documents up to the specified limit
+            // Sort the results based on sort_type
+            ...(sort_type === 'ASCENDING' ? [{ $sort: { 'sizeVariants.selling_price': 1 } }] : []),
+            ...(sort_type === 'DECENDING' ? [{ $sort: { 'sizeVariants.selling_price': -1 } }] : []),
+            { $skip: skip },// Skip documents for pagination
+            { $limit: 12 },
+          ],
+        },
+      },
+    ])
+    // console.log(products[0].matchedResults)
 
-    const totalProducts = await Product.countDocuments({ isPublished: true });
-
+    const totalProducts = products[0]?.count[0]?.count;
     const totalPages = Math.ceil(totalProducts / limit);
 
-    const categories = await Category.find();
-
     res.status(200).json(success("OK", {
-      categories,
-      products,
+      products: products[0].matchedResults,
       pagination: {
         page_no: page,
         per_page: limit,
@@ -118,7 +215,7 @@ exports.getAllPublishedProducts = async (req, res) => {
       res.statusCode),
     );
   } catch (err) {
-    console.error(err);
+    console.log(err)
     return res.status(500).json(error("Something went wrong", res.statusCode));
   }
 };
@@ -308,11 +405,12 @@ exports.getProductsByCategoryId = async (req, res) => {
           // Retrieve matched results based on selling price range
           matchedResults: [// Filter documents based on selling price range
             { $match: { 'sizeVariants.selling_price': { $gt: Number(from), $lt: Number(to) } } },
-            { $skip: skip },// Skip documents for pagination
-            { $sample: { size: limit } },// Randomly select documents up to the specified limit
+            { $sample: { size: 99999 } },// Randomly select documents up to the specified limit
             // Sort the results based on sort_type
             ...(sort_type === 'ASCENDING' ? [{ $sort: { 'sizeVariants.selling_price': 1 } }] : []),
             ...(sort_type === 'DECENDING' ? [{ $sort: { 'sizeVariants.selling_price': -1 } }] : []),
+            { $skip: skip },// Skip documents for pagination
+            { $limit: 12 },
           ],
         },
       },
@@ -614,7 +712,7 @@ exports.addColorAndItsSizeVariant = async (req, res) => {
     console.log(err)
     await session.abortTransaction();
     return res.status(500).json(error("Something went wrong", res.statusCode));
-  } 
+  }
 };
 
 // @desc   Add size variant of a color variant
